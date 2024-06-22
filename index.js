e25a3edd6f014bf708595d2ccd418c85c3048a4c
jmm7gameover <jmm7gameover@gmail.com>
2024-06-22 14:41:43 -0600

    creando el modelo de los rides

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 // database connection
 const mongoose = require("mongoose");
-const db = mongoose.connect("mongodb+srv://josephme5712:9a1Ao5AEy09ewGbC@cluster0.m5sfesz.mongodb.net/teachers");
+const db = mongoose.connect("mongodb+srv://josephme5712:9a1Ao5AEy09ewGbC@cluster0.m5sfesz.mongodb.net/DB_Aventones");
 
 // parser for the request body (required for the POST and PUT methods)
 const bodyParser = require("body-parser");
