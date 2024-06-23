ef8a9fa615ce5c86eeb9a0f62f12b3550e2318e8
anjemo2903 <molinajesus2003@gmail.com>
2024-06-22 19:22:52 -0600

    Se actualizo user

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 // database connection
 const mongoose = require("mongoose");
-const db = mongoose.connect("mongodb+srv://molinajesus2003:weJyz3uFbpRRcg2M@cluster0.orvrvph.mongodb.net/");
+const db = mongoose.connect("mongodb+srv://molinajesus2003:weJyz3uFbpRRcg2M@cluster0.orvrvph.mongodb.net/DB_Aventados");
 
 // parser for the request body (required for the POST and PUT methods)
 const bodyParser = require("body-parser");
@@ -16,10 +16,16 @@ app.use(cors({
 }));
 
 const {
-  userPost
+  userPost,
+  userGet,
+  userDelete,
+  userPatch
 } = require("./Controllers/userController.js");
 
 app.post("/api/user",userPost)
+app.get("/api/user",userGet)
+app.delete("/api/user",userDelete)
+app.patch("/api/user",userPatch)
 
 
 app.listen(3001, () => console.log(`Example app listening on port 3001!`))
